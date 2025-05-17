06e509dcedc2ba6be77a4ffdb7ef730457c4e757
akshar <akshau0123@gmail.com>
2025-05-17 20:17:12 +0530

    deployment updated

--- gateway-service/src/main/java/org/dev/nextgen/gatewayservice/config/GatewayConfig.java
+++ gateway-service/src/main/java/org/dev/nextgen/gatewayservice/config/GatewayConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -29,14 +30,14 @@ public class GatewayConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
         corsConfig.setAllowCredentials(true);
 
-        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source =
-                new org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
